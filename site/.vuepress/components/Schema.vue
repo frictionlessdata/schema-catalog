0b39586613ffd17092caeb8f0fc09325fac8413a
monikappv <monika.popova@datopian.com>
2020-05-26 11:30:13 +0300

    [schema-page][m]: Add if statements so that the layout looks good regardless of schema titles' length. Also add a go back button a do small css alignments and fixes.

--- site/.vuepress/components/Schema.vue
+++ site/.vuepress/components/Schema.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="pt-20">
+    <div class="container py-8">
+      <a href="/catalog/" class="text-black text-lg lg:px-20"> < Go back </a>
+    </div>
     <div class="flex lg:flex-row flex-col">
       <div class="lg:w-2/3 lg:px-20 pb-8 lg:pb-0">
         <a :href="$page.frontmatter.schemaUrl" target="_blank">
@@ -8,18 +11,22 @@
         </a>
       </div>
       <div class="lg:w-1/3">
-        <div class="container flex flex-row">
+        <div v-if="$page.frontmatter.title.length <= 20" class="container flex flex-row">
           <h2 class="border border-solid border-black text-black px-2 w-8 rounded"> {{ $page.frontmatter.country }} </h2>
           <h2 class="pl-4"> {{ $page.frontmatter.title }} </h2>
         </div>
-        <p class="text-xl"> {{ $page.frontmatter.description }} </p>
-        <div v-if="$page.frontmatter.repoUrl" class="container">
-          <img src="/repo-icon.svg" class="inline-block w-6"/>
-          <a :href="$page.frontmatter.repoUrl" target="_blank" class="inline-block text-lg text-black pl-4">Repository</a>  
+        <div v-if="$page.frontmatter.title.length > 20" class="container flex flex-col">
+          <h2 class="border border-solid border-black text-black px-2 w-8 rounded"> {{ $page.frontmatter.country }} </h2>
+          <h2 class=""> {{ $page.frontmatter.title }} </h2>
         </div>
-        <div v-if="$page.frontmatter.docsUrl" class="container pt-2">
-          <img src="/docs-icon.svg" class="inline-block w-6"/>
-          <a :href="$page.frontmatter.docsUrl" target="_blank" class="inline-block text-lg text-black pl-4">Docs</a>  
+        <p class="text-xl"> {{ $page.frontmatter.description }} </p>
+        <div v-if="$page.frontmatter.repoUrl" class="container flex flex-row">
+          <img src="/repo-icon.svg" class="w-6"/>
+          <a :href="$page.frontmatter.repoUrl" target="_blank" class="text-lg text-black pl-4">Repository</a>  
+        </div>
+        <div v-if="$page.frontmatter.docsUrl" class="pt-2 container flex flex-row">
+          <img src="/docs-icon.svg" class="w-6"/>
+          <a :href="$page.frontmatter.docsUrl" target="_blank" class="text-lg text-black pl-4">Docs</a>  
         </div>
       </div>
     </div>
